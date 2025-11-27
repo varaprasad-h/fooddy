2cf6c591df915dabf94a4f96376a0ac323a8908e
mkishore <mkishore@speshway.com>
2025-11-27 13:00:21 +0530

    vdeploye1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import UserDashboard from '../components/userDashboard'
+import UserDashboard from '../components/UserDashboard'
 import OwnerDashboard from '../components/OwnerDashboard'
 import DeliveryBoy from '../components/DeliveryBoy'
 
