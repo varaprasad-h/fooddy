277ffb1c7f43145950eba46e8afd1e798cd472fc
laddu344 <valmiki.9398@gmail.com>
2025-12-30 19:40:32 +0530

    Add new allowed origin for CORS

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,7 @@ const defaultAllowed = [
   "http://localhost:5174",
   "http://localhost:5175",
   "http://localhost:5180",
+  "http://98.91.188.66",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
   "http://127.0.0.1:5175",
